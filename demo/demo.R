65958f8f6cf71aed33c423a1146090f3b76716cb
Eytan Bakshy <ebakshy@alerer-mbp-2.local.DHCP.thefacebook.com>
2015-10-07 21:53:24 -0700

    Provide distributional stats
    This adds distributional stats to the simulator output, e.g.,
    min/max/mean/median/IQR/95% intervals for each statistic (e.g., regret,
    MSE, knows_best_arm).  This also changes the output format to be "long"
    such that each row contains distributional information on a particular
    metric.

--- demo/demo.R
+++ demo/demo.R
@@ -1,33 +1,50 @@
 library(ggplot2)
+library(dplyr)
 
-tmp <- read.csv("demo.tsv", sep = "\t")
+#tmp <- read.csv("big_demo1s.tsv", sep = "\t")
+tmp <- read.csv("lil_demo1.tsv", sep = "\t")
 
-ggplot(
-    subset(tmp, T == max(T)),
-    aes(x = AvgCumulativeRegret, AvgMSE, color = Algorithm)
-) +
-    geom_point() +
-    scale_x_log10() +
-    scale_y_log10() +
-    facet_grid(Delay ~ .)
-ggsave("regret_mse_tradeoff_final_trial.jpg", height = 8, width = 12)
+tmp %>%
+  filter(metric=='cumulative_regret', T>3) %>%
+qplot(
+x = T/1000, y = q50, color = algorithm,fill=algorithm, data=.,geom='line',
+ylab='Cumulative regret', xlab='T/1000', main='Regret distribution') +
+geom_ribbon(aes(ymin=q25+0.001,ymax=q75+0.001), alpha=0.5) + 
+geom_ribbon(aes(ymin=q025+0.001,ymax=q975+0.001), alpha=0.25) + 
+geom_ribbon(aes(ymin=min,ymax=max), alpha=0.01) +
+    facet_grid(delay ~ algorithm) + theme_bw()
+    
+tmp %>%
+  filter(metric=='cumulative_regret', T>3) %>%
+qplot(
+x = T/1000, y = q50, color = algorithm,fill=algorithm, data=.,geom='line',
+ylab='Cumulative regret', xlab='T/1000', main='Regret distribution') +
+geom_ribbon(aes(ymin=q25+0.001,ymax=q75+0.001), alpha=0.5) + 
+geom_ribbon(aes(ymin=q025+0.001,ymax=q975+0.001), alpha=0.25) + 
+geom_ribbon(aes(ymin=min,ymax=max), alpha=0.01) +
+   theme_bw()
+    
 
-ggplot(
-    subset(tmp, T > 3),
-    aes(x = AvgCumulativeRegret, AvgMSE, color = Algorithm)
-) +
-    geom_point() +
-    scale_x_log10() +
-    scale_y_log10() +
-    facet_grid(Delay ~ .)
-ggsave("regret_mse_tradeoff.jpg", height = 8, width = 12)
+    
+tmp %>%
+  filter(metric=='knows_best', T>3) %>%
+qplot(
+x = T, y = mean, color = algorithm,fill=algorithm, data=.,geom='line') + 
+    facet_grid(delay ~ .) + theme_bw()
 
-ggplot(
-    subset(tmp, T > 3),
-    aes(x = T, y = AvgReward, color = Algorithm)
-) +
-    geom_line() +
-    facet_grid(Delay ~ .)
+tmp %>%
+  filter(metric=='se_best', T>3, delay==1,
+  algorithm != 'UCB1Tuned(MLELearner(0.500000, 0.250000))') %>%
+qplot(
+x = T, y = q50, color = algorithm,fill=algorithm, data=.,geom='line',
+ylab='cumulative regret', main='regret distribution',log='y') +
+geom_ribbon(aes(ymin=q25+0.001,ymax=q75+0.001), alpha=0.5) + 
+geom_ribbon(aes(ymin=q025+0.001,ymax=q975+0.001), alpha=0.25) + 
+geom_ribbon(aes(ymin=min,ymax=max), alpha=0.01) +
+    facet_grid(.~algorithm ) + theme_bw()
+    
+    
+ 
 ggsave("average_reward.jpg", height = 8, width = 12)
 
 ggplot(
@@ -73,3 +90,27 @@ ggplot(
     scale_y_log10() +
     facet_grid(Delay ~ .)
 ggsave("average_se_best.jpg", height = 8, width = 12)
+
+
+
+
+ggplot(
+    subset(tmp, T == max(T)),
+    aes(x = AvgCumulativeRegret, AvgMSE, color = Algorithm)
+) +
+    geom_point() +
+    scale_x_log10() +
+    scale_y_log10() +
+    facet_grid(Delay ~ .)
+ggsave("regret_mse_tradeoff_final_trial.jpg", height = 8, width = 12)
+
+ggplot(
+    subset(tmp, T > 3),
+    aes(x = AvgCumulativeRegret, AvgMSE, color = Algorithm)
+) +
+    geom_point() +
+    scale_x_log10() +
+    scale_y_log10() +
+    facet_grid(Delay ~ .)
+ggsave("regret_mse_tradeoff.jpg", height = 8, width = 12)
+
--- src/06_simulators/02_average_game_statistics.jl
+++ src/06_simulators/02_average_game_statistics.jl
@@ -1,41 +1,17 @@
 abstract AverageGameStatistics
 
 immutable CoreAverageGameStatistics <: AverageGameStatistics
+    S::Int
     T::Int
-    avg_reward::Vector{Float64}
-    avg_instantaneous_regret::Vector{Float64}
-    avg_cumulative_regret::Vector{Float64}
-    avg_knows_best::Vector{Float64}
-    avg_mse::Vector{Float64}
-    avg_se_best::Vector{Float64}
+    games::Vector{CoreSingleGameStatistics}
 
-    function CoreAverageGameStatistics(T::Integer)
-        avg_reward = Array(Float64, T)
-        fill!(avg_reward, 0.0)
-
-        avg_instantaneous_regret = Array(Float64, T)
-        fill!(avg_instantaneous_regret, 0.0)
-
-        avg_cumulative_regret = Array(Float64, T)
-        fill!(avg_cumulative_regret, 0.0)
-
-        avg_knows_best = Array(Float64, T)
-        fill!(avg_knows_best, 0.0)
-
-        avg_mse = Array(Float64, T)
-        fill!(avg_mse, 0.0)
-
-        avg_se_best = Array(Float64, T)
-        fill!(avg_se_best, 0.0)
+    function CoreAverageGameStatistics(S::Integer, T::Integer)
+        games = Array(CoreSingleGameStatistics, S)
 
         return new(
+            S,
             T,
-            avg_reward,
-            avg_instantaneous_regret,
-            avg_cumulative_regret,
-            avg_knows_best,
-            avg_mse,
-            avg_se_best,
+            games
         )
     end
 end
@@ -47,25 +23,38 @@ function dump(
     delay::Integer,
     statistics::CoreAverageGameStatistics,
 )
+    S = statistics.S
     # Print out data in TSV format
     for t in 1:statistics.T
-        @printf(
-            io,
-            "%s\t%d\t%d\t%d\t%f\t%f\t%f\t%f\t%f\t%f\n",
-            string(algorithm),
-            bandit_id,
-            delay,
-            t,
-            statistics.avg_reward[t],
-            statistics.avg_instantaneous_regret[t],
-            statistics.avg_cumulative_regret[t],
-            statistics.avg_knows_best[t],
-            statistics.avg_mse[t],
-            statistics.avg_se_best[t],
-        )
+       # we assume all games have same fields, and use the first to get names
+       fields = fieldnames(statistics.games[1])
+       for metric = fields
+         if metric != :T && metric != :chosen_arm
+           vals = [Float64(getfield(statistics.games[s], metric)[t]) for s in 1:S]
+           val_quantiles = quantile(vals, [0.025, 0.25, 0.5, 0.75, 0.975])
+           min_val, max_val = minimum(vals), maximum(vals)
+           mean_val = mean(vals)
+           @printf(
+              io,
+              "%s\t%d\t%d\t%d\t%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",
+              string(algorithm),
+              bandit_id,
+              delay,
+              t,
+              string(metric),
+              mean_val,
+              min_val,
+              val_quantiles[1],
+              val_quantiles[2],
+              val_quantiles[3],
+              val_quantiles[4],
+              val_quantiles[5],
+              max_val,
+           )
+        end
+      end
     end
-
-    return
+  return
 end
 
 function update!(
@@ -74,20 +63,6 @@ function update!(
     s::Integer,
 )
     α = 1 / s
-
-    for t in 1:avgs.T
-        avgs.avg_reward[t] =
-            (1 - α) * avgs.avg_reward[t] + α * obs.reward[t]
-        avgs.avg_instantaneous_regret[t] =
-            (1 - α) * avgs.avg_instantaneous_regret[t] + α * obs.instantaneous_regret[t]
-        avgs.avg_cumulative_regret[t] =
-            (1 - α) * avgs.avg_cumulative_regret[t] + α * obs.cumulative_regret[t]
-        avgs.avg_knows_best[t] =
-            (1 - α) * avgs.avg_knows_best[t] + α * obs.knows_best[t]
-        avgs.avg_mse[t] = (1 - α) * avgs.avg_mse[t] + α * obs.mse[t]
-        avgs.avg_se_best[t] =
-            (1 - α) * avgs.avg_se_best[t] + α * obs.se_best[t]
-    end
-
+    avgs.games[s] = deepcopy(obs)
     return
 end
